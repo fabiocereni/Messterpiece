3617ad887845b36f1726cb04b38dd3035f4b14d6
fabio.cereni <fabio.cereni@supsi.ch>
2026-01-11 01:53:31 +0100

    added pause menu logic into tutorial

--- Assets/Scripts/PauseMenuLogic.cs
+++ Assets/Scripts/PauseMenuLogic.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuLogic : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public GameObject pausePanel;
+    public bool isPaused = false;
+
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pausePanel.SetActive(true); 
+        Time.timeScale = 0f; // congelo il tempo (nemici fermi, animazioni ferme)
+        isPaused = true;
+
+        Cursor.lockState = CursorLockMode.None; // mostro il cursore
+        Cursor.visible = true;
+    }
+
+    public void ResumeGame()
+    {
+        pausePanel.SetActive(false); 
+        Time.timeScale = 1f;
+        isPaused = false;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void QuitToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
     }
 }
