3d7964a35f9b31b4a537d33c971d246ae940187a
Federico Molteni <federicomolteni@MacBook-Pro-di-Federico-2.local>
2026-01-18 15:10:17 +0100

    now the damage indicator is not stuck in the middle

--- Assets/Scripts/UI/DamageIndicator.cs
+++ Assets/Scripts/UI/DamageIndicator.cs
@@ -6,14 +6,31 @@ public class DamageIndicator : MonoBehaviour
     public Vector3 DamageLocation;
     public Transform PlayerObject;
     public Transform DamageImagePivot;
+
+    public CanvasGroup DamageImageCanvas;
+    public float FadeStartTime, FadeTime;
+    private float maxFadeTime;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (FadeStartTime > 0)
+        {
+            FadeStartTime -= Time.deltaTime;
+        } else
+        {
+            FadeTime -= Time.deltaTime;
+            DamageImageCanvas.alpha = FadeTime / maxFadeTime;
+            if (FadeTime <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+        
+        DamageLocation.y = PlayerObject.position.y;
         Vector3 dir = (DamageLocation - PlayerObject.position).normalized;
         float angle = (Vector3.SignedAngle(dir, PlayerObject.forward, Vector3.up));
         DamageImagePivot.transform.localEulerAngles = new Vector3(0, 0, angle);
